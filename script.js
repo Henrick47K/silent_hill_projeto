c8a0db74c9f48c44bb8cfdb3b09685a5ae90c016
Henrique Duarte <contatohenrick47@gmail.com>
2026-05-07 21:19:50 -0300

    update

---
+++ script.js
@@ -0,0 +1,26 @@
+function toggleMenu() {
+
+    const menu = document.querySelector(".menu-lateral");
+
+    menu.classList.toggle("ativo");
+
+}
+
+function login() {
+
+    let email = document.getElementById("email").value;
+    let senha = document.getElementById("senha").value;
+
+    if(email === "admin@gmail.com" && senha === "1234") {
+
+        localStorage.setItem("logado", "true");
+        localStorage.setItem("usuario", email);
+
+        window.location.href = "index.html";
+
+    } else {
+
+        alert("Login inválido");
+
+    }
+}
